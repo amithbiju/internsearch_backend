b7ca242e5ac2eeafe037f5069156b975375acd2b
Anagha <anaghaadma@gmail.com>
2025-02-28 21:14:07 +0530

    done10

--- models/company.js
+++ models/company.js
@@ -4,7 +4,9 @@ const companySchema = new mongoose.Schema({
     cmpUserId: { type: String, required: true },
     cmpName: { type: String, required:true},
     cmpImg: { type: String, required: true },
+    address: { type: Object, required: true },
+    phno: {type: Number, required:true}
 })
-
+//has been changed
 const companyModel = mongoose.models.company || mongoose.model('company',companySchema)
 export default companyModel;
