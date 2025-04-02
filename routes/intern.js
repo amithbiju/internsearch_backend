f876e5e51614750314b6f9ed2c98ededf8f85861
Amith Biju <amithbiju1@gmail.com>
2025-04-03 00:31:18 +0530

    amith_intern

--- routes/intern.js
+++ routes/intern.js
@@ -5,10 +5,12 @@ const internRouter = express.Router();
 import {
   getinterns,
   getRecommendedForStudents,
+  getMatchedStudents,
 } from "../controllers/internControler.js";
 import authUser from "../middleware/auth.js";
 
 internRouter.post("/get", getinterns);
+internRouter.post("/getmatched", getMatchedStudents);
 internRouter.post("/getrecomend", authUser, getRecommendedForStudents);
 
 export default internRouter;
